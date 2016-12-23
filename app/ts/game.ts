57b718fdf482174e33784e40205529f082913a54
severjason <ssever2010@gmail.com>
2016-12-23 20:20:30 +0200

    added new methods and properties

--- app/ts/game.ts
+++ app/ts/game.ts
@@ -5,26 +5,59 @@ class Arcomage {
     private _playerTwo:Player;
     private _cardsQuantity:number;
     private _cards:ArcomageCards;
-    
+    private _playerOneTurn:boolean;
+    private _playerTwoTurn:boolean;
 
     constructor(params:Param, cards:ArcomageCards) {
-        this._playerOne = new Player(params.firstPlayerName, params.firstPlayerValues,params.maxValues, params.mainCanvasValues);
+        this._playerOne = new Player(params.firstPlayerName, params.firstPlayerValues, params.maxValues, params.mainCanvasValues);
         this._playerTwo = new Player(params.secondPlayerName, params.secondPlayerValues, params.maxValues, params.mainCanvasValues);
         this._cardsQuantity = params.cardsQuantity;
         this._cards = cards;
         this._params = params;
+        this._playerOneTurn = true;
+        this._playerTwoTurn = false;
     }
 
-    get firstPlayer() {
+    get params():Param {
+        return this._params;
+    }
+
+    get firstPlayer():Player {
         return this._playerOne;
     }
 
-    get secondPlayer() {
+    get firstPlayerTurn():boolean {
+        return this._playerOneTurn
+    }
+
+    get secondPlayer():Player {
         return this._playerTwo;
+    }
+
+    get secondPlayerTurn():boolean {
+        return this._playerTwoTurn
+    }
+
+    firstPlayerMoved():void {
+        this._playerOneTurn = false;
+        this._playerTwoTurn = true;
+    }
+
+    secondPlayerMoved():void {
+        this._playerTwoTurn = false;
+        this._playerOneTurn = true;
     }
 
     get cardsValues():any {
         return this._params.cardsValues;
+    }
+
+    get cards():ArcomageCards {
+        return this._cards;
+    }
+
+    get cardsQuantity():number {
+        return this._cardsQuantity
     }
 
     applyCard(cardName:string, player:Player, enemy:Player) {
@@ -32,14 +65,17 @@ class Arcomage {
         //this.CARDS[cardName].isActive = false;
     }
 
-    allotCards(player:Player):boolean {
-        let cardsNames:Array<string> = this._cards.names;
-        for (let i = 0; player.cards.length < this._cardsQuantity; i++) {
-            let random:number  = Math.floor(Math.random() * (cardsNames.length));
-            let randomCard:any = this._cards.getSingleCard(cardsNames[random]);
 
-            if (!randomCard.isActive) {
-                randomCard.isActive = true;
+    allotCards(player:Player):boolean {
+        let cardsNames:Array<string> = this.cards.names;
+
+        for (let i = 0; player.cards.length < this.cardsQuantity; i++) {
+
+            let random:number = Math.floor(Math.random() * (cardsNames.length));
+            let randomCard:any = this.cards.getSingleCard(cardsNames[random]);
+
+            if (!this.cards.isActive(cardsNames[random])) {
+                this.cards.activate(cardsNames[random]);
                 player.updateCards(randomCard);
             }
             else this.allotCards(player);
@@ -50,16 +86,28 @@ class Arcomage {
     drawCards(canvas:Canvas) {
         let that = this;
         for (let i = 0; i < that.firstPlayer.cards.length; i++) {
-            let playerCard = that.firstPlayer.cards[i];
-            let playerCardObject = playerCard.object;
-            playerCardObject.left = (that.cardsValues.width + 2 * this.cardsValues.padding) * i;
+            let playerCardObject:IGroup = that.firstPlayer.cards[i].object;
+            let paddingLeft = (i === 0)
+                ? this.cardsValues.padding
+                : (that.cardsValues.width + 2 * this.cardsValues.padding) * i + this.cardsValues.padding;
+            playerCardObject.setLeft(paddingLeft);
+            playerCardObject.setOpacity(1);
             canvas.fabricElement.add(playerCardObject);
-            canvas.fabricElement.renderAll();
-
         }
     }
-    
-    
+
+    updateResources(playerOneSources:any, playerTwoSources:any):void {
+        let sources:Array<string> = Object.keys(this.params.relations);
+        let newResourcesPlayerOne:any = {};
+        let newResourcesPlayerTwo:any = {};
+        for (let i = 0; i < sources.length; i++) {
+            newResourcesPlayerOne[this.params.relations[sources[i]]] = playerOneSources[sources[i]];
+            newResourcesPlayerTwo[this.params.relations[sources[i]]] = playerTwoSources[sources[i]];
+        }
+        this.firstPlayer.updateResources(newResourcesPlayerOne);
+        this.secondPlayer.updateResources(newResourcesPlayerTwo);
+    }
+
 
 }
 
