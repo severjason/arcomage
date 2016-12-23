6187cc9f203f61d82a3237ff70f4335ebc3a2a27
severjason <ssever2010@gmail.com>
2016-12-23 20:16:18 +0200

    Add params to events class

--- app/ts/loader.ts
+++ app/ts/loader.ts
@@ -97,7 +97,7 @@ class Loader {
                     that.params.secondPlayerName,
                     that.game.secondPlayer,
                     that.params.mainCanvasValues);
-                return new Events(that.cards, that.canvas, that.game);
+                return new Events(that.params, that.cards, that.canvas, that.game);
             })
             .then(function (events) {
                 that.events = events;
