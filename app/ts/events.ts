3187fd5226c792a462ffaae9b8cf7ccb669fa099
severjason <ssever2010@gmail.com>
2016-12-25 23:34:58 +0200

    Added events to discard field

--- app/ts/events.ts
+++ app/ts/events.ts
@@ -40,59 +40,135 @@ class Events {
         this.addEvents();
     }
 
-    addEvents():void {
+    private shakeCard(cardName:string):void {
+        let that = this;
+        let cardObject:IGroup = this.cards.getCardObject(cardName);
+        let cardLeftValue:number = cardObject.getLeft();
+        cardObject.animate({
+            left: cardLeftValue - that.params.cardsValues.padding
+        }, {
+            onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+            duration: 100,
+            onComplete: function () {
+                cardObject.animate({
+                    left: cardLeftValue + that.params.cardsValues.padding
+                }, {
+                    onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+                    duration: 100,
+                    onComplete: function () {
+                        cardObject.animate({
+                            left: cardLeftValue
+                        }, {
+                            onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+                            duration: 100
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private addEvents():void {
         let that = this;
         for (let i = 0; i < this.cards.names.length; i++) {
             let cardName:string = this.cards.names[i];
             let card:any = this.cards.getSingleCard(cardName);
             let cardObject:IGroup = this.cards.getCardObject(cardName);
-            cardObject.on("mousedown", function () {
-                if (that.cards.cardCanBeUsed(cardName, that.game.firstPlayer) && that.game.firstPlayerTurn) {
-                    that.game.firstPlayerMoved();
-                    let basicValue:any = {
-                        "top": cardObject.top,
-                        "left": cardObject.left
-                    };
-                    that.cards.deactivate(cardName);
-                    cardObject.animate({
-                        "top": 100,
-                        "left": (that.canvas.width - that.params.cardsValues.width) / 2
-                    }, {
-                        onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
-                        easing: fabric.util.ease.easeInCubic,
-                        duration: 500,
-                        onComplete: function () {
-                            that.game.applyCard(cardName, that.game.firstPlayer, that.game.secondPlayer);
+
+            let applyCard = ():void => {
+                if (that.game.firstPlayerTurn) {
+                    if (!that.cards.cardCanBeUsed(cardName, that.game.firstPlayer)) {
+                        that.shakeCard(cardName)
+                    }
+                    else {
+                        if (that.cards.isActive(cardName)) {
+
+                            that.game.firstPlayerMoved();
+                            let basicValue:any = {
+                                "top": cardObject.getTop(),
+                                "left": cardObject.getLeft()
+                            };
+                            that.cards.deactivate(cardName);
                             cardObject.animate({
-                                "top": 0,
-                                "opacity": 0
+                                "top": 100,
+                                "left": (that.canvas.width - that.params.cardsValues.width) / 2
                             }, {
                                 onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+                                easing: fabric.util.ease.easeInCubic,
                                 duration: 500,
                                 onComplete: function () {
-                                    that.canvas.fabricElement.remove(cardObject);
-                                    that.firstPlayer.removeCard(card);
-                                    cardObject.setTop(basicValue.top);
-                                    //cardObject.setLeft(-1000); // stupid hack
+                                    that.game.applyCard(cardName, that.game.firstPlayer, that.game.secondPlayer);
+                                    cardObject.animate({
+                                        "top": 0,
+                                        "opacity": 0
+                                    }, {
+                                        onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+                                        duration: 500,
+                                        onComplete: function () {
+                                            that.canvas.fabricElement.remove(cardObject);
+                                            that.firstPlayer.removeCard(card);
+                                            cardObject.setTop(basicValue.top);
 
-                                    let eventPromise:Promise<any> = new Promise((resolve, reject) => {
-                                        (that.game.allotCards(that.firstPlayer))
-                                            ? resolve()
-                                            : reject("Can`t set card active status to false!");
-                                    });
-                                    eventPromise.then(() => {
-                                        that.game.drawCards(that.canvas);
-                                    }).then(()=> {
-                                        that.game.updateResources(that.firstPlayer.sources, that.secondPlayer.sources);
-                                        that.canvas.fabricElement.renderAll();
-                                        that.game.secondPlayerMoved();
+                                            let eventPromise:Promise<any> = new Promise((resolve, reject) => {
+                                                (that.game.allotCards(that.firstPlayer))
+                                                    ? resolve()
+                                                    : reject("Can`t set card active status to false!");
+                                            });
+                                            eventPromise.then(() => {
+                                                that.game.drawCards(that.canvas, that.game.firstPlayer);
+                                            }).then(()=> {
+                                                that.game.updateResources(that.firstPlayer.sources, that.secondPlayer.sources);
+                                                that.canvas.fabricElement.renderAll();
+                                                that.game.secondPlayerMoved();
+                                            });
+                                        }
                                     });
                                 }
                             });
                         }
-                    });
+                    }
                 }
-            });
+            };
+
+            let discardCard = ():void => {
+                if (that.game.firstPlayerTurn) {
+                    if (that.cards.isActive(cardName)) {
+                        that.game.firstPlayerMoved();
+                        let cardObjectTop:number = cardObject.getTop();
+                        that.cards.deactivate(cardName);
+                        cardObject.animate({
+                            "top": 0,
+                            "opacity": 0
+                        }, {
+                            onChange: that.canvas.fabricElement.renderAll.bind(that.canvas.fabricElement),
+                            duration: 500,
+                            onComplete: function () {
+                                that.canvas.fabricElement.remove(cardObject);
+                                that.firstPlayer.removeCard(card);
+                                cardObject.setTop(cardObjectTop);
+
+                                let eventPromise:Promise<any> = new Promise((resolve, reject) => {
+                                    (that.game.allotCards(that.firstPlayer))
+                                        ? resolve()
+                                        : reject("Can`t set card active status to false!");
+                                });
+                                eventPromise.then(() => {
+                                    that.game.drawCards(that.canvas, that.game.firstPlayer);
+                                    that.game.secondPlayerMoved();
+                                });
+                            }
+                        });
+                    }
+
+                }
+            };
+
+            for (let i = 0; i < 6; i++) {
+                cardObject._objects[i].on("mousedown", applyCard);
+            }
+            for (let i = 6; i < 8; i++) {
+                cardObject._objects[i].on("mousedown", discardCard);
+            }
         }
     }
 }
